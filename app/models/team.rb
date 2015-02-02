9931e25955c26d56df50cb0f2f402109b6d71806
palmbeachjeff <palmbeachjeff@gmail.com>
2015-02-02 08:18:53 -0500

    Can't seed database with spreads

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,11 @@
 class Team < ActiveRecord::Base
 	belongs_to :city
 	has_one :coach  
-	validates :name, uniqueness: true
+	#validates :name, uniqueness: true
 	has_many :home_games, class_name: "Game", foreign_key: "home_team_id"
 	has_many :away_games, class_name: "Game", foreign_key: "away_team_id"
     #need Team_id in Game Table
-    validates :conference, :division, :name, :presence, :true
+    #validates :conference, :division, :name, :presence, :true
 
     def division_rivals
     	Team.where(conference: conference,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+=begin
+
+class AddGameData < ActiveRecord::Migration
+require 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +10,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+256.times do |i|
+	Game.create(date: "Game ##{i}", home_team_id: "Game ##{i}", away_team_id: "Game ##{i}", 
+				home_score: "Game ##{i}", away_score: "Game ##{i}")
+	end
+
+
+	def down
+	Game.delete_all
+	end
+=end
+
+	
